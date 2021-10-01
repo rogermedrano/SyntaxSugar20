71cba62d934c2c0ef3c7e809e29798fe8f5f01c5
Roger Medrano <email@example.com>
2021-10-01 12:49:41 -0700

    SyntaxSugarEx

---
+++ SyntaxSugar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + "is less than nine.";
+            }
+            else
+            {
+                response = answer + " is greater than or equal to nine.";
+            }
+            { 
+                response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine.";
+            }
+            }
+    }
+}
